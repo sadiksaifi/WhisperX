7f1ec43e651c777a1ded66834af4521577d21884
Sadik Saifi <mail@sadiksaifi.dev>
2026-01-09 16:24:38 +0530

    Initial Commit

---
+++ whisperX/whisperXApp.swift
@@ -0,0 +1,17 @@
+//
+//  whisperXApp.swift
+//  whisperX
+//
+//  Created by Sadik Saifi on 09/01/26.
+//
+
+import SwiftUI
+
+@main
+struct whisperXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
